f2fe67bbbc9e2d22b963f5e707aef054f4c19907
Александр Пономаренко <ponomarenko.100299@gmail.com>
2023-03-15 10:29:11 +0300

    part 3 complete

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
+    def __str__(self) -> models.CharField():
         return self.question_text
 
     def was_published_recently(self) -> bool:
@@ -16,9 +16,11 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    def __str__(self):
-        return self.choice_text
-
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> models.CharField():
+        return self.choice_text
+
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse, Http404
+
+from .models import Question
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, World. You're at polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context=context)
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {question: question_id})
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    response = "You're looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're voting on question %s." % question_id)
